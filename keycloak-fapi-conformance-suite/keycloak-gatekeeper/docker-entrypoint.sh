114c332b183af9dd35c5ec953b54a389017e7ceb
brothwellIntegral <brothwell@integraltech.com.au>
2020-05-29 09:03:34 +1000

    fixed CRLF windows line endings in scripts to be LF

--- keycloak-fapi-conformance-suite/keycloak-gatekeeper/docker-entrypoint.sh
+++ keycloak-fapi-conformance-suite/keycloak-gatekeeper/docker-entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-/opt/keycloak-gatekeeper --config /opt/config.yml --listen 0.0.0.0:10443 --discovery-url https://${KEYCLOAK_FQDN}/auth/realms/${KEYCLOAK_REALM}
+/opt/keycloak-gatekeeper \
+    --config /opt/config.yml \
+    --listen 0.0.0.0:10443 \
+    --discovery-url https://${KEYCLOAK_FQDN}/auth/realms/${KEYCLOAK_REALM}
--- keycloak-fapi-conformance-suite/server-entrypoint.sh
+++ keycloak-fapi-conformance-suite/server-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 mvn package -DskipTests
-java -jar target/fapi-test-suite.jar -Djava.security.egd=file:/dev/./urandom --fintechlabs.base_url=https://${CONFORMANCE_SUITE_FQDN} --fintechlabs.devmode=true --fintechlabs.startredir=true
+java -jar target/fapi-test-suite.jar \
+    -Djava.security.egd=file:/dev/./urandom \
+    --fintechlabs.base_url=https://${KEYCLOAK_FQDN} \
+    --fintechlabs.devmode=true \
+    --fintechlabs.startredir=true
