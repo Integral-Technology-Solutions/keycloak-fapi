0582c3ce157728eaf4fc56ae3e22817df65a7c2b
brothwellIntegral <brothwell@integraltech.com.au>
2020-05-29 14:01:23 +1000

    cleanup, trying some different endpoints to solve issue getting JWK set from server configuration in all tests

--- keycloak-fapi-conformance-suite/server-entrypoint.sh
+++ keycloak-fapi-conformance-suite/server-entrypoint.sh
@@ -2,6 +2,6 @@
 mvn package -DskipTests
 java -jar target/fapi-test-suite.jar \
     -Djava.security.egd=file:/dev/./urandom \
-    --fintechlabs.base_url=https://host.docker.internal:8443 \
+    --fintechlabs.base_url=https://${CONFORMANCE_SUITE_FQDN} \
     --fintechlabs.devmode=true \
     --fintechlabs.startredir=true
