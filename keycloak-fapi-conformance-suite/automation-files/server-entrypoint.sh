9915559066c68f00c0b1e6db08e37cef4374bd37
Brendan Rothwell <brendan.rothwell@hpw.qld.gov.au>
2020-09-16 12:41:52 +1000

    added debug logging for frontChannel package in server container, fixed BrowserControl match URLs

--- keycloak-fapi-conformance-suite/automation-files/server-entrypoint.sh
+++ keycloak-fapi-conformance-suite/automation-files/server-entrypoint.sh
@@ -6,4 +6,5 @@ java -Xdebug -Xrunjdwp:transport=dt_socket,address=*:9999,server=y,suspend=n \
     -Djava.security.egd=file:/dev/./urandom \
     --fintechlabs.base_url=${CONFORMANCE_SERVER} \
     --fintechlabs.devmode=true \
-    --fintechlabs.startredir=true
+    --fintechlabs.startredir=true \
+    --logging.level.net.openid.conformance.frontChannel=DEBUG
